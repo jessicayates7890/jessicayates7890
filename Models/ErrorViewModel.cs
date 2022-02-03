8b9fa76d519c5fd9858b0a803b87a31e0d24c9e5
jessicayates7890 <60652495+jessicayates7890@users.noreply.github.com>
2022-02-03 10:39:03 +0000

    Added all the files so far via upload.
    Current this is just a working login screen with very bad CSS.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NotesApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
